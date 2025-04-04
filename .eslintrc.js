5249767c1c3f8a8c0a2060c1b0f64717861099d5
Anthony Rumsey <arumsey@adobe.com>
2025-04-04 11:53:10 -0400

    fix: Disable eslint rules under tools/importer

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,5 @@ module.exports = {
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
   },
-  ignorePatterns: ['tools/importeer/**/*'],
+  ignorePatterns: ['tools/importer/**/*'],
 };
