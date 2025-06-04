00905a3d2401dc42131362559d1fe071aa7da3e0
Doru Muntean <muntean@adobe.com>
2025-06-04 15:43:26 +0300

    Different selector

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,12 +1,12 @@
 export default function decorate(block) {
-  const divs = [...block.querySelectorAll('div')];
+  const divs = [...block.querySelectorAll(':scope > div > div')];
 
   if (divs.length < 2) return;
 
   const container = document.createElement('div');
-  container.append(...divs.slice(3));
+  container.append(...divs.slice(1));
 
-  if (!divs[1].querySelector('picture')) {
+  if (!divs[0].querySelector('picture')) {
     block.classList.add('no-image');
   }
 
