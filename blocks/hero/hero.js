4f68ae27d57f74f66fd3890652b429f098c25a9d
Doru Muntean <muntean@adobe.com>
2025-06-04 16:00:17 +0300

    Code review changes

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,14 +1,5 @@
 export default function decorate(block) {
-  const divs = [...block.querySelectorAll(':scope > div > div')];
-
-  if (divs.length < 2) return;
-
-  const container = document.createElement('div');
-  container.append(...divs.slice(1));
-
-  if (!divs[0].querySelector('picture')) {
+  if (!block.querySelector(':scope > div:first-child picture')) {
     block.classList.add('no-image');
   }
-
-  block.replaceChildren(divs[0], container);
 }
